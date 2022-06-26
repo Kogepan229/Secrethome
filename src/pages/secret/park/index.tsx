a8a7c311639e02725b8216a48a26bbd1e3ce6636
Kogepan25629 <kogepan25629@gmail.com>
2022-06-26 11:43:36 +0900

    add redirect

--- src/pages/secret/park/index.tsx
+++ src/pages/secret/park/index.tsx
@@ -1,6 +1,12 @@
 import type { NextPage } from 'next'
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
 
 const Park: NextPage = () => {
+    const router = useRouter()
+    useEffect(() => {
+        router.replace("/secret/park/contents")
+    }, [])
     return (<p>Here is park</p>)
 }
 
