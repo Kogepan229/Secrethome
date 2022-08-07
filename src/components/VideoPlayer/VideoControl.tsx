08b5579d5701217027899575d79691c099cf6d6c
Kogepan25629 <kogepan25629@gmail.com>
2022-08-07 13:07:47 +0900

    avoid unnecessary re-rendering

--- src/components/VideoPlayer/VideoControl.tsx
+++ src/components/VideoPlayer/VideoControl.tsx
@@ -1,4 +1,5 @@
-import { ReactEventHandler, ReactNode, RefObject, useEffect, useRef, useState } from "react";
+import React, { useMemo } from "react";
+import { ReactEventHandler, ReactNode, RefObject, useCallback, useEffect, useRef, useState } from "react";
 import css from "./VideoControl.module.scss"
 
 type Props = {
@@ -74,7 +75,9 @@ const VideoControl = (props: Props) => {
     }
   }
 
+  /*
   const switchPlayStop = () => {
+    console.log("click")
     if (isPlaying) {
       props.videoRef.current?.pause()
       setIsPlaying(false)
@@ -83,6 +86,17 @@ const VideoControl = (props: Props) => {
       setIsPlaying(true)
     }
   }
+  */
+  const switchPlayStop = useCallback(() => {
+    console.log("click")
+    if (isPlaying) {
+      props.videoRef.current?.pause()
+      setIsPlaying(false)
+    } else {
+      props.videoRef.current?.play()
+      setIsPlaying(true)
+    }
+  }, [isPlaying])
 
   const onChangeSlider = (e: any) => {
     if (!props.videoRef.current) return;
@@ -130,12 +144,12 @@ const VideoControl = (props: Props) => {
     return str
   }
 
-  const onVideoTimeUpdate = () => {
+  const onVideoTimeUpdate = useCallback(() => {
     setDisplayTime(getVideoTimeStr())
     setVideoCurrentTime(props.videoRef.current?.currentTime!)
-  }
+  }, [])
 
-  const onClickBack = () => {
+  const onClickBack = useCallback(() => {
     if (!props.videoRef.current) return;
     if (props.videoRef.current!.currentTime - 5 < 0) {
       props.videoRef.current!.currentTime = 0
@@ -143,9 +157,9 @@ const VideoControl = (props: Props) => {
       props.videoRef.current!.currentTime -= 5
     }
     onVideoTimeUpdate()
-  }
+  }, [onVideoTimeUpdate])
 
-  const onClickForward = () => {
+  const onClickForward = useCallback(() => {
     if (!props.videoRef.current) return;
     if (props.videoRef.current!.currentTime + 5 > props.videoRef.current.duration) {
       props.videoRef.current!.currentTime = props.videoRef.current.duration
@@ -153,9 +167,9 @@ const VideoControl = (props: Props) => {
       props.videoRef.current!.currentTime += 5
     }
     onVideoTimeUpdate()
-  }
+  }, [onVideoTimeUpdate])
 
-  const onClickMute = () => {
+  const onClickMute = useCallback(() => {
     if (!volume) return;
     if (isMute) {
       setIsMute(false)
@@ -166,14 +180,17 @@ const VideoControl = (props: Props) => {
       controlVolumeSliderRef.current!.value = "0"
       props.videoRef.current!.volume = 0
     }
-  }
+  }, [volume, isMute, controlVolumeSliderRef, props.videoRef])
 
   const onChangeVolume = (e: any) => {
+    if (e.target.value !== 0) {
+      setIsMute(false)
+    }
     setVolume(e.target.value)
     props.videoRef.current!.volume = e.target.value / 100
   }
 
-  const onClickFull = () => {
+  const onClickFull = useCallback(() => {
     if (isFullScreen) {
       document.exitFullscreen().then(() => {
         setIsfullScreen(false)
@@ -183,7 +200,7 @@ const VideoControl = (props: Props) => {
         setIsfullScreen(true)
       })
     }
-  }
+  }, [isFullScreen, props.videoContainerRef])
 
   const onControlsMousemove = () => {
     setIsCursorOnControls(true);
@@ -194,6 +211,7 @@ const VideoControl = (props: Props) => {
   }
 
   const VideoControlButton = ({children, className, onClick}: {children: ReactNode, className?: string, onClick?: ReactEventHandler<HTMLButtonElement>}) => {
+    //console.log(className)
     return(
       <button className={`${css.video_control_button} ${className ?? ""}`} onClick={onClick}>
         <div>
@@ -203,14 +221,63 @@ const VideoControl = (props: Props) => {
     )
   }
 
-  const ControlPlayStop = () => {
+  const ControlPlayStop = useMemo(() => {
+    return(
+      <VideoControlButton onClick={switchPlayStop}>
+        {(() => {
+          if (isPlaying) {
+            return <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M14,19H18V5H14M6,19H10V5H6V19Z" /></svg>
+          } else {
+            return <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M8 5v14l11-7z"/></svg>
+          }
+        })()}
+      </VideoControlButton>
+    )
+  }, [isPlaying, switchPlayStop])
+
+  /*
+  const ControlPlayStop = useMemo(() => {
+    console.log("kuso")
     if (isPlaying) {
       return <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M14,19H18V5H14M6,19H10V5H6V19Z" /></svg>
     } else {
       return <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M8 5v14l11-7z"/></svg>
     }
-  }
+  }, [isPlaying])
+  */
 
+  const ControlBack = useMemo(() => {
+    return (
+      <VideoControlButton className={css.video_control_skip} onClick={onClickBack}>
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12.5 3C17.15 3 21.08 6.03 22.47 10.22L20.1 11C19.05 7.81 16.04 5.5 12.5 5.5C10.54 5.5 8.77 6.22 7.38 7.38L10 10H3V3L5.6 5.6C7.45 4 9.85 3 12.5 3M9 12H15V14H11V16H13C14.11 16 15 16.9 15 18V20C15 21.11 14.11 22 13 22H9V20H13V18H9V12Z" /></svg>
+      </VideoControlButton>
+    )
+  }, [onClickBack])
+
+  const ControlForward = useMemo(() => {
+    return (
+      <VideoControlButton className={css.video_control_skip} onClick={onClickForward}>
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M11.5 3C14.15 3 16.55 4 18.4 5.6L21 3V10H14L16.62 7.38C15.23 6.22 13.46 5.5 11.5 5.5C7.96 5.5 4.95 7.81 3.9 11L1.53 10.22C2.92 6.03 6.85 3 11.5 3M9 12H15V14H11V16H13C14.11 16 15 16.9 15 18V20C15 21.11 14.11 22 13 22H9V20H13V18H9V12Z" /></svg>
+      </VideoControlButton>
+    )
+  }, [onClickForward])
+
+
+  const ControlMute = useMemo(() => {
+    return (
+      <VideoControlButton className={css.video_control_mute} onClick={onClickMute}>
+        {(() => {
+          if (volume != 0 && !isMute) {
+            return <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M3 9v6h4l5 5V4L7 9H3zm13.5 3c0-1.77-1.02-3.29-2.5-4.03v8.05c1.48-.73 2.5-2.25 2.5-4.02zM14 3.23v2.06c2.89.86 5 3.54 5 6.71s-2.11 5.85-5 6.71v2.06c4.01-.91 7-4.49 7-8.77s-2.99-7.86-7-8.77z"/></svg>
+          } else {
+            return <svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="24" height="24" viewBox="0 0 24 24"><path d="M3,9H7L12,4V20L7,15H3V9M16.59,12L14,9.41L15.41,8L18,10.59L20.59,8L22,9.41L19.41,12L22,14.59L20.59,16L18,13.41L15.41,16L14,14.59L16.59,12Z" /></svg>
+          }
+        })()}
+      </VideoControlButton>
+    )
+  }, [volume, isMute, onClickMute])
+
+  /*
   const ControlMute = () => {
     if (volume != 0 && !isMute) {
       return <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M3 9v6h4l5 5V4L7 9H3zm13.5 3c0-1.77-1.02-3.29-2.5-4.03v8.05c1.48-.73 2.5-2.25 2.5-4.02zM14 3.23v2.06c2.89.86 5 3.54 5 6.71s-2.11 5.85-5 6.71v2.06c4.01-.91 7-4.49 7-8.77s-2.99-7.86-7-8.77z"/></svg>
@@ -218,12 +285,37 @@ const VideoControl = (props: Props) => {
       return <svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="24" height="24" viewBox="0 0 24 24"><path d="M3,9H7L12,4V20L7,15H3V9M16.59,12L14,9.41L15.41,8L18,10.59L20.59,8L22,9.41L19.41,12L22,14.59L20.59,16L18,13.41L15.41,16L14,14.59L16.59,12Z" /></svg>
     }
   }
+  */
 
+  const ControlFullScreen = useMemo(() => {
+    return (
+      <VideoControlButton onClick={onClickFull}>
+        {(() => {
+          if (isFullScreen) {
+            return <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M5 16h3v3h2v-5H5v2zm3-8H5v2h5V5H8v3zm6 11h2v-3h3v-2h-5v5zm2-11V5h-2v5h5V8h-3z"/></svg>
+          } else {
+            return <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M7 14H5v5h5v-2H7v-3zm-2-4h2V7h3V5H5v5zm12 7h-3v2h5v-5h-2v3zM14 5v2h3v3h2V5h-5z"/></svg>
+          }
+        })()}
+      </VideoControlButton>
+    )
+  }, [isFullScreen, onClickFull])
+
+  /*
   const ControlFullScrenn = () => {
     if (isFullScreen) {
       return <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M5 16h3v3h2v-5H5v2zm3-8H5v2h5V5H8v3zm6 11h2v-3h3v-2h-5v5zm2-11V5h-2v5h5V8h-3z"/></svg>
     } else {
       return <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M7 14H5v5h5v-2H7v-3zm-2-4h2V7h3V5H5v5zm12 7h-3v2h5v-5h-2v3zM14 5v2h3v3h2V5h-5z"/></svg>
+    }
+  }
+  */
+
+  const getNowTime = () => {
+    if (props.videoRef.current) {
+      return props.videoRef.current.currentTime
+    }else {
+      return 0
     }
   }
 
@@ -241,19 +333,11 @@ const VideoControl = (props: Props) => {
         </div>
         <div className={`${css.video_controls} ${isCursorOutVideo && !isCursorOnControls && isPlaying ? css.hide: ""}`}>
           <div className={css.video_left_controls}>
-            <VideoControlButton onClick={switchPlayStop}>
-              {ControlPlayStop()}
-            </VideoControlButton>
-            <VideoControlButton className={css.video_control_skip} onClick={onClickBack}>
-              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12.5 3C17.15 3 21.08 6.03 22.47 10.22L20.1 11C19.05 7.81 16.04 5.5 12.5 5.5C10.54 5.5 8.77 6.22 7.38 7.38L10 10H3V3L5.6 5.6C7.45 4 9.85 3 12.5 3M9 12H15V14H11V16H13C14.11 16 15 16.9 15 18V20C15 21.11 14.11 22 13 22H9V20H13V18H9V12Z" /></svg>
-            </VideoControlButton>
-            <VideoControlButton className={css.video_control_skip} onClick={onClickForward}>
-              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M11.5 3C14.15 3 16.55 4 18.4 5.6L21 3V10H14L16.62 7.38C15.23 6.22 13.46 5.5 11.5 5.5C7.96 5.5 4.95 7.81 3.9 11L1.53 10.22C2.92 6.03 6.85 3 11.5 3M9 12H15V14H11V16H13C14.11 16 15 16.9 15 18V20C15 21.11 14.11 22 13 22H9V20H13V18H9V12Z" /></svg>
-            </VideoControlButton>
+            {ControlPlayStop}
+            {ControlBack}
+            {ControlForward}
             <div className={css.video_volume_controller}>
-              <VideoControlButton className={css.video_control_mute} onClick={onClickMute}>
-                <ControlMute></ControlMute>
-              </VideoControlButton>
+              {ControlMute}
               <div className={css.video_control_volume}>
                 <input className={css.video_control_volume_slider} ref={controlVolumeSliderRef} onChange={onChangeVolume} type="range" max="100"></input>
                 <div className={css.video_control_volume_base}></div>
@@ -265,9 +349,7 @@ const VideoControl = (props: Props) => {
             </div>
           </div>
           <div className={css.video_right_controls}>
-            <VideoControlButton onClick={onClickFull}>
-              {ControlFullScrenn()}
-            </VideoControlButton>
+            {ControlFullScreen}
           </div>
         </div>
       </div>
