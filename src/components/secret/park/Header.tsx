4e674b31e186d40f301eb9d4c3272f7df4e8218a
Kogepan25629 <kogepan25629@gmail.com>
2022-08-09 18:20:09 +0900

    create Header component

---
+++ src/components/secret/park/Header.tsx
@@ -0,0 +1,15 @@
+import Link from "next/link"
+import css from "./Header.module.scss"
+
+const Header = () => {
+  return (
+    <div className={css.header}>
+      <Link href="/secret/park/">
+        <div className={css.header_logo}>Secret Home</div>
+      </Link>
+      <Link href="/secret/park/admin/add_content"><button>追加</button></Link>
+    </div>
+  )
+}
+
+export default Header
