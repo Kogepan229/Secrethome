1b3f4b4b52a03cb25b62cc7b8db478ac03383e3b
Kogepan25629 <kogepan25629@gmail.com>
2022-08-09 18:22:04 +0900

    add sidebar and header in contents page

--- src/pages/secret/park/contents/index.tsx
+++ src/pages/secret/park/contents/index.tsx
@@ -6,10 +6,14 @@ import { DB } from "util/sql"
 import css from "styles/pages/secret/park/contents/contents.module.scss"
 import Link from 'next/link';
 import { getContentTags, Tag } from 'util/secret/park/tags';
+import Header from 'components/secret/park/Header';
+import SideBar from 'components/secret/park/SideBar';
 
 type Props = {
-  page_num: number,
-  contents: {id: string, title: string, description: string, tags: Tag[], updated_at: string}[],
+  page_num: number;
+  contents: {id: string, title: string, description: string, tags: Tag[], updated_at: string}[];
+  tags: {tag: Tag, count: number}[]
+
 }
 
 const Contents: NextPage<Props> = (props) => {
@@ -19,10 +23,13 @@ const Contents: NextPage<Props> = (props) => {
 
   return (
     <SecretParkLayout>
-      <div className={css.container}>
-        <Link href="/secret/park/admin/add_content"><button>追加</button></Link>
-        <div className={css.contents_main}>
-          {contents}
+      <div className={css.body_container}>
+        <Header/>
+        <div className={css.container}>
+          <SideBar tags={props.tags}/>
+          <div className={css.contents_main}>
+            {contents}
+          </div>
         </div>
       </div>
     </SecretParkLayout>
@@ -32,11 +39,10 @@ const Contents: NextPage<Props> = (props) => {
 export default Contents
 
 export const getServerSideProps: GetServerSideProps = async (context: GetServerSidePropsContext) => {
-  //console.log(context.query.content_id)
-  let props: Props = {page_num: 0, contents: []};
+  let props: Props = {page_num: 0, contents: [], tags: []};
   props.page_num = context.query.page ? Number(context.query.page) : 0;
 
-  let result = await DB.query<any[]>(`select count(*) from park_contents`);
+  //let result = await DB.query<any[]>(`select count(*) from park_contents`);
   //console.log("num", result[0]['count(*)'])
 
   let result2 = await DB.query<any[]>(`select id, title, description, updated_at from park_contents limit ${props.page_num * 20}, 20`);
@@ -46,7 +52,19 @@ export const getServerSideProps: GetServerSideProps = async (context: GetServerS
     let tags = await getContentTags(data[i].id)
     props.contents[i] = {id: data[i].id, title: data[i].title, description: data[i].description, tags: tags, updated_at: data[i].updated_at}
   }
-  //console.log(props)
+
+  let resultTag1 = await DB.query<Tag[]>(`select id, name from park_tags`);
+  const dataTag = JSON.parse(JSON.stringify(resultTag1)) as Tag[];
+  if (resultTag1.length > 0) {
+    let tags = await Promise.all(dataTag.map(async value => {
+      let resultTag2 = await DB.query<any[]>(`select count(*) from park_tags_of_contents where tag_id='${value.id}'`);
+      return {tag: value, count: resultTag2[0]['count(*)']}
+    }))
+    tags.sort((a: {tag: Tag, count: number}, b: {tag: Tag, count: number}) => {
+      return b.count - a.count
+    })
+    props.tags = tags;
+  }
 
   return {
     props: props,
