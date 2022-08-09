0f2f250c787d8f57d7cd4c74eaef43f3216a58da
Kogepan25629 <kogepan25629@gmail.com>
2022-08-09 18:21:11 +0900

    change variable name

--- src/pages/secret/park/test/sidebar.tsx
+++ src/pages/secret/park/test/sidebar.tsx
@@ -20,14 +20,14 @@ export const getServerSideProps: GetServerSideProps = async (context: GetServerS
   //console.log(context.query.content_id)
   //await DB.query("insert into park_contents values ('test1', 'titile dayo', 'description dayo', '2022-11-1 11:11:11', '2022-11-3 13:13:13')")
 
-  let result = await DB.query<Tag[]>(`select id, name from park_tags`);
-  const data = JSON.parse(JSON.stringify(result)) as Tag[];
-  if (result.length == 0) {
+  let resultTag1 = await DB.query<Tag[]>(`select id, name from park_tags`);
+  const dataTag = JSON.parse(JSON.stringify(resultTag1)) as Tag[];
+  if (resultTag1.length == 0) {
     return { props: {tags: []} };
   } else {
-    let tags = await Promise.all(data.map(async value => {
-      let result2 = await DB.query<any[]>(`select count(*) from park_tags_of_contents where tag_id='${value.id}'`);
-      return {tag: value, count: result2[0]['count(*)']}
+    let tags = await Promise.all(dataTag.map(async value => {
+      let resultTag2 = await DB.query<any[]>(`select count(*) from park_tags_of_contents where tag_id='${value.id}'`);
+      return {tag: value, count: resultTag2[0]['count(*)']}
     }))
     tags.sort((a: {tag: Tag, count: number}, b: {tag: Tag, count: number}) => {
       return b.count - a.count
