4c70c4651771429ad09220022d067932bac7aa7b
Kogepan25629 <kogepan25629@gmail.com>
2022-08-11 00:18:53 +0900

    fix className and add hide option in Sidebar

--- src/components/secret/park/SideBar.tsx
+++ src/components/secret/park/SideBar.tsx
@@ -1,17 +1,19 @@
 import Link from "next/link"
 import { Tag } from "util/secret/park/tags"
+import { DB } from "util/sql"
 import css from "./SideBar.module.scss"
 
 type Props = {
   tags: {tag: Tag, count: number}[]
   className?: string;
+  isHide?: boolean;
 }
 
 const SideBar = (props: Props) => {
   const Tags = props.tags.map(value => {
     return (
       <Link href={`/secret/park/tag/${value.tag.id}`} key={value.tag.id}>
-        <div className={`${css.sidebar_item_tag} ${props.className ?? ""}`}>
+        <div className={css.sidebar_item_tag}>
           <p className={css.sidebar_item_tag_name}>{value.tag.name}</p>
           <div className={css.sidebar_item_tag_count}>
             <p>{value.count}</p>
@@ -21,10 +23,8 @@ const SideBar = (props: Props) => {
     )
   })
 
-
-
   return (
-    <div className={css.sidebar}>
+    <div className={`${css.sidebar} ${props.className ?? ""}`} data-is-hide={props.isHide ?? false}>
       <div className={css.sidebat_title}>タグ一覧</div>
       {Tags}
     </div>
