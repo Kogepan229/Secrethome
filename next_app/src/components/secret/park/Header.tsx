65f126b2bf45391ca736cd5ead2ef3f0fa221adc
Kogepan25629 <kogepan25629@gmail.com>
2022-11-25 14:27:30 +0000

    Fix link to top page in header

--- next_app/src/components/secret/park/Header.tsx
+++ next_app/src/components/secret/park/Header.tsx
@@ -4,7 +4,7 @@ import css from "./Header.module.scss"
 const Header = () => {
   return (
     <div className={css.header}>
-      <Link href="/secret/park/">
+      <Link href="/secret/park/contents">
         <div className={css.header_logo}>Secret Home</div>
       </Link>
       <Link href="/secret/park/admin/add_content"><button>追加</button></Link>
