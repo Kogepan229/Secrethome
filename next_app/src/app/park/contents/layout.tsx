ab25397122aaa4cad8f274826ffb66640efadf16
Kogepan25629 <kogepan25629@gmail.com>
2023-01-23 18:15:14 +0900

    add layout.tsx for SSR

---
+++ next_app/src/app/park/contents/layout.tsx
@@ -0,0 +1,9 @@
+export const dynamic = 'force-dynamic'
+
+const ContentsLayout = ({children}: {children: React.ReactNode}) => {
+  return (
+    <>{children}</>
+  );
+}
+
+export default ContentsLayout
