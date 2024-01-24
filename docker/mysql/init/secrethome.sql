b714ae9a84d6a0febd79907ff520c05493d84bec
Kogepan25629 <kogepan25629@gmail.com>
2024-01-24 19:10:47 +0900

    Change DB structure

--- docker/mysql/init/secrethome.sql
+++ docker/mysql/init/secrethome.sql
@@ -1,89 +1,39 @@
--- MySQL dump 10.13  Distrib 8.0.30, for Linux (x86_64)
---
--- Host: localhost    Database: secrethome
--- ------------------------------------------------------
--- Server version	8.0.30-0ubuntu0.22.04.1
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Table structure for table `park_contents`
---
-
-DROP TABLE IF EXISTS `park_contents`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `park_contents` (
+DROP TABLE IF EXISTS `rooms`;
+CREATE TABLE `rooms` (
   `id` varchar(64) NOT NULL,
+  `name` varchar(64) NOT NULL,
+  `description` varchar(4096) DEFAULT NULL,
+  `room_type` varchar(64) NOT NULL,
+  `key` varchar(64) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY (`key`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+DROP TABLE IF EXISTS `contents`;
+CREATE TABLE `contents` (
+  `id` varchar(64) NOT NULL,
+  `room_id` varchar(64) NOT NULL,
   `title` varchar(128) DEFAULT NULL,
   `description` varchar(4096) DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `park_contents`
---
-
-
---
--- Table structure for table `park_tags`
---
-
-DROP TABLE IF EXISTS `park_tags`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `park_tags` (
+DROP TABLE IF EXISTS `tags`;
+CREATE TABLE `tags` (
   `id` varchar(64) NOT NULL,
+  `room_id` varchar(64) NOT NULL,
   `name` varchar(32) NOT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  UNIQUE KEY (`room_id`, `name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `park_tags`
---
-
-
---
--- Table structure for table `park_tags_of_contents`
---
-
-DROP TABLE IF EXISTS `park_tags_of_contents`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `park_tags_of_contents` (
+DROP TABLE IF EXISTS `tags_of_contents`;
+CREATE TABLE `tags_of_contents` (
   `content_id` varchar(64) NOT NULL,
   `tag_id` varchar(64) NOT NULL,
   `priority` int unsigned NOT NULL,
   PRIMARY KEY (`content_id`,`tag_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `park_tags_of_contents`
---
-
-
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2022-08-18 21:17:06
