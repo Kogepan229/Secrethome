f3cab508694afc847ebced7314c65bdbbcd50431
Kogepan25629 <kogepan25629@gmail.com>
2024-02-28 13:48:20 +0900

    [front] Refactor getContentTagsData

--- next_app/src/features/tags/tags.ts
+++ next_app/src/features/tags/tags.ts
@@ -9,29 +9,13 @@ export type SidebarTagsData = { tag: TagData; count: number }[]
 
 export const getContentTagsData = async (contentID: string) => {
   const con = await getDBConnection()
-  const [rows, _] = await con.query(`SELECT tag_id, priority FROM tags_of_contents WHERE content_id=?`, [contentID])
-  const data1: { tag_id: string; priority: number }[] = JSON.parse(JSON.stringify(rows))
-
-  type ContentTag = TagData & {
-    priority: number
-  }
-  let _tags: ContentTag[] = []
-
-  for (let value of data1) {
-    let [rows2, _] = await con.query(`SELECT id, name FROM tags WHERE id=?`, [value.tag_id])
-    let data2: ContentTag[] = JSON.parse(JSON.stringify(rows2))
-    _tags.push({ id: data2[0].id, name: data2[0].name, priority: value.priority })
-  }
-
+  const [rows, _] = await con.query(
+    `SELECT tags.id, tags.name FROM tags JOIN tags_of_contents ON tags.id = tags_of_contents.tag_id WHERE tags_of_contents.content_id=? ORDER BY tags_of_contents.priority`,
+    [contentID]
+  )
   con.end()
 
-  _tags.sort((a: ContentTag, b: ContentTag) => {
-    return a.priority - b.priority
-  })
-
-  let tags = _tags.map(value => {
-    return { id: value.id, name: value.name } as TagData
-  })
+  const tags: TagData[] = JSON.parse(JSON.stringify(rows))
 
   return tags
 }
